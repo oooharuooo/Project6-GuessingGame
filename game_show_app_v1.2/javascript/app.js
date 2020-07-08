3fa45aeaf0a15dd3894e3de84c5452550f0bf087
Dat Thong <haruthong@gmail.com>
2020-07-08 00:19:19 -0700

    Update app.js

--- game_show_app_v1.2/javascript/app.js
+++ game_show_app_v1.2/javascript/app.js
@@ -3,7 +3,7 @@ const phrase = document.querySelector('#phrase');
 const btnReset = document.querySelector('.btn__reset');
 const missed = 0;
 const phrases = ['JAVASCRIPT','FUNCTION','OBJECT','ARRAY','LOOP'];
-
+let listPhrase;
 
 // return a random phrase from an array
 const getRandomPhraseAsArray = arr => {
@@ -15,18 +15,32 @@ const getRandomPhraseAsArray = arr => {
 const randomLetter = getRandomPhraseAsArray(phrases).split('');
 
 // adds the letters of a string to display
-const displayPhrases = arr => {
+// const displayPhrases = arr => {
+const a = function() {
     const ulPhrase = document.querySelector('#phrase ul'); 
-     for (let value of arr) {
-        const listPhrase = document.createElement('li');
+     for (let value of randomLetter) {
+        listPhrase = document.createElement('li');
         listPhrase.textContent = value;
         listPhrase.className = 'letter'
-        ulPhrase.appendChild(listPhrase);
-        
-        return listPhrase.textContent;
+        ulPhrase.appendChild(listPhrase);      
     } 
 }
-const displayPhrase = displayPhrases(randomLetter);
+a();
+const letter = document.querySelectorAll('.letter')
+
+for(let i = 0; i < letter.length; i++) {
+    // const letters = letter[i].textContent;
+    // console.log(letters)
+    
+    qwerty.addEventListener('click', e => {
+        if(e.target.tagName === 'BUTTON'){
+            letter[i].style.color = 'red';
+        e.target.style.display = 'none';}
+    });
+}
+
+// }
+// const displayPhrase = displayPhrases(randomLetter);
 
 // check if a letter is in the Phrase
 const checkLetter = button => {
@@ -46,8 +60,3 @@ const display = document.querySelector('#overlay');
 // })
 
 // listen for the onscreen keyboard to be clicked
-qwerty.addEventListener('click', e => {
-    if(e.target.tagName === 'BUTTON'){
-        
-    e.target.style.display = 'none';}
-});
