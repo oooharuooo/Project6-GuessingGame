bed65df7661fd5affbc94ad2a74cf3c40f3f7f3d
Dat Thong <haruthong@gmail.com>
2020-07-11 19:15:37 -0700

    Update app.js

--- game_show_app_v1.2/javascript/app.js
+++ game_show_app_v1.2/javascript/app.js
@@ -3,7 +3,9 @@ const phrase = document.querySelector('#phrase');
 const btnReset = document.querySelector('.btn__reset');
 const missed = 0;
 const phrases = ['JAVASCRIPT','FUNCTION','OBJECT','ARRAY','LOOP'];
-let listPhrase;
+const buttons = document.querySelectorAll('.keyrow button')
+
+
 
 // return a random phrase from an array
 const getRandomPhraseAsArray = arr => {
@@ -15,32 +17,23 @@ const getRandomPhraseAsArray = arr => {
 const randomLetter = getRandomPhraseAsArray(phrases).split('');
 
 // adds the letters of a string to display
-// const displayPhrases = arr => {
-const a = function() {
+const displayPhrases = arr => {
     const ulPhrase = document.querySelector('#phrase ul'); 
-     for (let value of randomLetter) {
-        listPhrase = document.createElement('li');
-        listPhrase.textContent = value;
-        listPhrase.className = 'letter'
-        ulPhrase.appendChild(listPhrase);      
-    } 
-}
-a();
-const letter = document.querySelectorAll('.letter')
-
-for(let i = 0; i < letter.length; i++) {
-    // const letters = letter[i].textContent;
-    // console.log(letters)
     
-    qwerty.addEventListener('click', e => {
-        if(e.target.tagName === 'BUTTON'){
-            letter[i].style.color = 'red';
-        e.target.style.display = 'none';}
-    });
+     for (let value of randomLetter) {
+        const listPhrase = document.createElement('li');
+        listPhrase.textContent = value;
+        listPhrase.className = 'letter';
+        ulPhrase.appendChild(listPhrase);   
+    } 
+    return ulPhrase.children
 }
 
-// }
-// const displayPhrase = displayPhrases(randomLetter);
+
+const displayPhrase = displayPhrases(randomLetter);
+const lists = Array.from(displayPhrase)
+let list = lists.map(list => list.textContent)
+
 
 // check if a letter is in the Phrase
 const checkLetter = button => {
@@ -52,11 +45,19 @@ const checkWin = () => {
 
 };
 
-const display = document.querySelector('#overlay');
-    display.style.display = 'none';
+
 // listen for the start game button to be pressed
-// btnReset.addEventListener('click',() => {
-    
-// })
+btnReset.addEventListener('click',() => {
+    const display = document.querySelector('#overlay');
+    display.style.display = 'none';
+})
 
 // listen for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', e => {
+    const button = e.target
+    if(button.tagName === 'BUTTON'){
+      if(list.includes(button.textContent.toUpperCase())){
+        lists.map(list => list.classList.add('show'))
+      };
+    e.target.style.display = 'none';}
+});
